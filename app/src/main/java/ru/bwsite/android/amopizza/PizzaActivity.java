9c7ee007266d0432e78a5c7c2fdd4e1128edab93
Пользователь 1 <1>
2019-10-25 15:08:31 +0800

    #изменения от 25-10-19

--- app/src/main/java/ru/bwsite/android/amopizza/PizzaActivity.java
+++ app/src/main/java/ru/bwsite/android/amopizza/PizzaActivity.java
@@ -8,7 +8,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PizzaActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -19,6 +21,9 @@ public class PizzaActivity extends AppCompatActivity {
     private String[] priceArray1 = new String[]{"290", "430", "590", "840"};
     private String[] priceArray2 = new String[]{"370", "495", "640", "890"};
     private String[] priceArray3 = new String[]{"390", "530", "690", "990"};
+    /*private Map<String, String> price_size1 = new HashMap<>();
+    private Map<String, String> price_size2 = new HashMap<>();
+    private Map<String, String> price_size3 = new HashMap<>();*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,7 @@ public class PizzaActivity extends AppCompatActivity {
         productsList.add(new Products("Пицца","Нью-Йорк", R.drawable.newjork, "Ветчина, грибы, пицца-соус, сыр моцарелла", priceArray2));
         productsList.add(new Products("Пицца","Пепперони", R.drawable.pepperoni, "Колбаски Пепперони, сыр моцарелла, пицца-соус", priceArray2));
         productsList.add(new Products("Пицца","Вегетарианская", R.drawable.vegetarianskaya, "Томаты, грибы, перец болгарский, пицца-соус, сыр фета, сыр моцарелла, маслины", priceArray2));
+        productsList.add(new Products("Пицца","Морская", R.drawable.morskaya, "Пицца соус, креветки, сладкий перец, маслины, сыр моцарелла", priceArray3));
 
     }
 
--- app/src/main/java/ru/bwsite/android/amopizza/Products.java
+++ app/src/main/java/ru/bwsite/android/amopizza/Products.java
@@ -2,6 +2,7 @@ package ru.bwsite.android.amopizza;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Products {
     String gr_name;
@@ -9,6 +10,7 @@ public class Products {
     int imageResourceID;
     String description;
     String[] price_size = new String[4];
+    //Map<String, String> price_size;
 
     public Products(String gr_name, String title, int imageResourceID, String description, String[] price_size) {
         this.title = title;
