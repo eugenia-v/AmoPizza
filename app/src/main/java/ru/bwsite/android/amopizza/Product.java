e28f354668249fdc9adf0c1778a09b5f6fc0b21e
art.vershinin <art.vershinin@playa.ru>
2019-11-03 16:25:11 +0800

    #Try with retrofit

--- app/src/main/java/ru/bwsite/android/amopizza/Product.java
+++ app/src/main/java/ru/bwsite/android/amopizza/Product.java
@@ -1,6 +1,7 @@
 package ru.bwsite.android.amopizza;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Product {
     int id;
@@ -9,9 +10,9 @@ public class Product {
     String desc;
     String label_text;
     String label_color;
-    SizePrice[] size_price;
+    List<SizePrice> size_price;
 
-    public Product(int id, String name, String img_url, String desc, String label_text, String label_color, SizePrice[] size_price) {
+    public Product(int id, String name, String img_url, String desc, String label_text, String label_color, List<SizePrice> size_price) {
         this.id = id;
         this.name = name;
         this.img_url = img_url;
