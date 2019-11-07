7a0ac248f24080719406f234bc8f2476c6ea7c9c
Пользователь 1 <1>
2019-11-07 18:47:57 +0800

    #пытаюсь сделать отображение кнопок динамичным

--- app/src/main/java/ru/bwsite/android/amopizza/PizzaActivity/AdapterPizzaActivity.java
+++ app/src/main/java/ru/bwsite/android/amopizza/PizzaActivity/AdapterPizzaActivity.java
@@ -1,6 +1,8 @@
 package ru.bwsite.android.amopizza.PizzaActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +24,8 @@ import ru.bwsite.android.amopizza.R;
 public class AdapterPizzaActivity extends RecyclerView.Adapter<AdapterPizzaActivity.MyViewHolder> {
     private List<Product> productList;
     private PizzaActivity context;
+    private static final int VIEW_ORDINARY = 0;
+    private static final int VIEW_WITH_EXTRA_TEXT_VIEW = 1;
 
     // Provide a suitable constructor (depends on the kind of dataset)
     public AdapterPizzaActivity(List<Product> pizza, PizzaActivity context) {
@@ -33,10 +37,17 @@ public class AdapterPizzaActivity extends RecyclerView.Adapter<AdapterPizzaActiv
     @Override
     public AdapterPizzaActivity.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_pizza, parent, false);
 
-        return new MyViewHolder(view, context);
+        if(viewType == VIEW_WITH_EXTRA_TEXT_VIEW) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.pizza_buttons, parent, false);
+            return new PizzaButtonsHolder(itemView);
+        } else {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_pizza, parent, false);
+
+            return new MyViewHolder(view);
+        }
     }
 
     // Replace the contents of a view (invoked by the layout manager)
@@ -50,16 +61,22 @@ public class AdapterPizzaActivity extends RecyclerView.Adapter<AdapterPizzaActiv
                 .into(holder.imageView);
         holder.textViewDescription.setText(productList.get(position).getDesc());
 
+        if(getItemViewType(position)==VIEW_WITH_EXTRA_TEXT_VIEW){
         int buttonCount = productList.get(position).getSize_price().size();
+        Log.d("createButton1",String.valueOf(position));
 
-        for (int i = 0; i < buttonCount; i++) {
-            holder.priceText.setText(productList.get(position).getSize_price().get(i).getPrice());
-            if (productList.get(position).getSize_price().get(i).getSize() != null) {
+        for (int i = 0; i < buttonCount-2; i++) {
+            Log.d("createButton2",String.valueOf(position));
+            if(productList.get(position).getSize_price().get(i).getSize() != null){
+                Log.d("createButton3",String.valueOf(position));
+                holder.priceText.setText(productList.get(position).getSize_price().get(i).getPrice());
                 ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 Button myButton = new Button(context);
                 myButton.setText(productList.get(position).getSize_price().get(i).getSize());
                 holder.linearLayout.addView(myButton, lp);
+
             }
+        }
         }
 
     }
@@ -70,16 +87,23 @@ public class AdapterPizzaActivity extends RecyclerView.Adapter<AdapterPizzaActiv
         return productList.size();
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if(position == 0) {
+            return VIEW_WITH_EXTRA_TEXT_VIEW;
+        } else {
+            return VIEW_ORDINARY;
+        }
+    }
+
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         private ImageView imageView;
         private TextView textView;
         private TextView textViewDescription;
         private LinearLayout linearLayout;
         private TextView priceText;
-        private Button button;
 
-
-        public MyViewHolder(View itemView, Context context) {
+        public MyViewHolder(View itemView) {
             super(itemView);
             textView = itemView.findViewById(R.id.title);
             imageView = itemView.findViewById(R.id.image);
