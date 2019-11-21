b702d33a550645365e20ede92a22514af8b884cc
art.vershinin <art.vershinin@playa.ru>
2019-11-21 22:38:21 +0800

    #Событие на нажатие кнопки

---
+++ app/src/main/java/ru/bwsite/android/amopizza/DataObjects/Groupp.java
@@ -0,0 +1,15 @@
+package ru.bwsite.android.amopizza.DataObjects;
+
+import java.util.List;
+
+import ru.bwsite.android.amopizza.DataObjects.Product;
+
+public class Groupp {
+    public String gr_name;
+    public List<Product> products;
+
+    public Groupp(String gr_name, List<Product> products) {
+        this.gr_name = gr_name;
+        this.products = products;
+    }
+}
